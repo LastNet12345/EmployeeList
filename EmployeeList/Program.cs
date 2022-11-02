3d7ef072a8b0286143b335170cfc35673fe2a203
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-02 13:19:44 +0100

    REmove code in main

--- EmployeeList/Program.cs
+++ EmployeeList/Program.cs
@@ -4,28 +4,7 @@
     {
         static void Main(string[] args)
         {
-          // Program p = new Program();
-           Robot robot = new Robot("Kalle", 55);
-            Console.WriteLine(robot.GetFirstName());
-            robot.SetFirstName("Anna");
-            Console.WriteLine(robot.GetFirstName());
-
-            robot.Length = 45;
-            int length = robot.Length;
-
-           //Robot robot2 = new Robot("Nisse",50);
-          // Robot robot3 = new Robot();
-
-
-
-           // robot.Name = "Kalle";
-
-           // string name = robot.Name; 
-            
-           // Robot robot2 = new Robot();
-            //robot.Name = "Nisse";
-
-            //string name2 = robot.Name;
+       
         }
     }
 }
