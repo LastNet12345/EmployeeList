3dc8e1cdbf210be742f0f381d45ddfe31cec1278
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-02 11:50:03 +0100

    Robot begin

--- EmployeeList/Program.cs
+++ EmployeeList/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          // Program p = new Program();
+           Robot robot = new Robot("Kalle", 55);
+            Console.WriteLine(robot.GetFirstName());
+            robot.SetFirstName("Anna");
+            Console.WriteLine(robot.GetFirstName());
+
+            robot.Length = 45;
+            int length = robot.Length;
+
+           //Robot robot2 = new Robot("Nisse",50);
+          // Robot robot3 = new Robot();
+
+
+
+           // robot.Name = "Kalle";
+
+           // string name = robot.Name; 
+            
+           // Robot robot2 = new Robot();
+            //robot.Name = "Nisse";
+
+            //string name2 = robot.Name;
         }
     }
 }
---
+++ EmployeeList/Robot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    public class Robot
+    {
+        private string firstname;
+        private int age;
+
+        //private int length;
+
+        //public int Length
+        //{
+        //    get { return length; }
+        //    set { length = value; }
+        //}
+
+        public int Length { get; set; }
+       
+        // public int Length2;
+
+
+        //public Robot()
+        //{
+
+        //}
+
+        public Robot(string name, int age)
+        {
+            //Validate
+            this.age = age;
+            firstname = name;
+        }
+
+        public void SetFirstName(string newFirstName)
+        {
+            //Validate
+            firstname = newFirstName;
+        }
+
+        public string GetFirstName()
+        {
+            return firstname;
+        }
+    }
+}
