cebc0165924ed0ccf34008812cd8ef47f7faa2b5
David Nokto <david.nokto@gmail.com>
2022-11-21 12:10:32 +0100

    Snabbfix på DataRow errors

--- EmployeeList.MSTest/EmployeeTests.cs
+++ EmployeeList.MSTest/EmployeeTests.cs
@@ -19,24 +19,24 @@ namespace EmployeeList.MSTest
             Assert.AreEqual(expected, actual);
         }
 
-        //[DataTestMethod]
-        //[DataRow(2)]
-        //[DataRow(20)]
-        //[DataRow(200)]
-        //[DataRow(24999)]
-        //public void SalaryLevel_Get_ShouldReturn_SalaryLevelJunior2(uint salary)
-        //{
-        //    // Arrange
-        //    const string name = "Kalle";
-        //    var emp = new Employee(name, salary);
-        //    var expected = SalaryLevel.Junior;
+        [TestMethod]
+        [DataRow(2)]
+        [DataRow(20)]
+        [DataRow(200)]
+        [DataRow(24999)]
+        public void SalaryLevel_Get_ShouldReturn_SalaryLevelJunior2(int salary)
+        {
+            // Arrange
+            const string name = "Kalle";
+            var emp = new Employee(name, (uint)salary);
+            var expected = SalaryLevel.Junior;
 
-        //    // Act
-        //    var actual = emp.SalaryLevel;
+            // Act
+            var actual = emp.SalaryLevel;
 
-        //    // Assert
-        //    Assert.AreEqual(expected, actual);
-        //}
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
 
 
     }
